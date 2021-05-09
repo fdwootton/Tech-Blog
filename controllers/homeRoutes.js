dbe5b159cec013f11825b95e039e3536b8a7115c
Felicia Wootton <felicia.wootton@gmail.com>
2021-05-09 12:52:56 -0500

    edited homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['user_name'],
         },
       ],
     });
@@ -33,7 +33,7 @@ router.get('/posts/:id', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['user_name'],
         },
       ],
     });
