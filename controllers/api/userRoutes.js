221aa0b13d76da94afffff829e698ffdb2461850
Felicia Wootton <felicia.wootton@gmail.com>
2021-05-14 22:07:32 -0500

    logout functionality working

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,10 +3,15 @@ const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const userData = await User.create({
+      user_name: req.body.user_name,
+      email: req.body.email,
+      password: req.body.password
+    });
 
     req.session.save(() => {
       req.session.user_id = userData.id;
+      req.session.user_name = userData.user_name;
       req.session.logged_in = true;
 
       res.status(200).json(userData);
