d24c897fdc8cafea924f5154b091c6093dee2246
Felicia Wootton <felicia.wootton@gmail.com>
2021-05-14 22:56:52 -0500

    got data to dashboard page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,10 +28,36 @@ router.get('/', (req, res) => {
         });
 });
 
+// router.get('/dashboard', (req, res) => {
+
+//   res.render('dashboard');
+// });
+
+
+router.get('/dashboard', async (req, res) => {
+    try {
+        // Find the logged in user based on the session ID
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: ['user_name'],
+            include: [{ model: Post }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('dashboard', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+  }
+});
+
+
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-      res.redirect('/profile');
+      res.redirect('/dashboard');
       return;
     }
   
